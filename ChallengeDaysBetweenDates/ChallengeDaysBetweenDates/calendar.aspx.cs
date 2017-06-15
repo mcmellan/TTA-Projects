1d41d9a6d2b0eb3e5697d4d6c3178be4cc13c492
mcmellan <macabreleg@gmail.com>
2017-06-15 16:49:16 -0700

    calendar days

---
+++ ChallengeDaysBetweenDates/ChallengeDaysBetweenDates/calendar.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeDaysBetweenDates
+{
+    public partial class calendar : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+
+
+            TimeSpan myTimeSpan = TimeSpan.Parse("1");
+
+            resultLabel.Text = Calendar1.SelectedDate.Subtract(Calendar2.SelectedDate).TotalDays.ToString();
+
+            resultLabel.Text = Math.Abs(Calendar2.SelectedDate.Subtract(Calendar1.SelectedDate).TotalDays).ToString();
+
+            
+
+          
+
+        }
+    }
+}
